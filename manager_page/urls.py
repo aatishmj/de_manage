4dc55db4f2756549ac7e6aa920367a36c4cad731
aatishmj <aatishjawalkar13@gmail.com>
2023-09-22 17:33:22 +0530

    modified:   .DS_Store 	modified:   db.sqlite3 	modified:   manager_page/__pycache__/urls.cpython-310.pyc 	modified:   manager_page/__pycache__/views.cpython-310.pyc 	modified:   manager_page/urls.py 	modified:   manager_page/views.py 	renamed:    templates/style.css -> static/style.css 	modified:   templates/.DS_Store 	renamed:    templates/Admin.html -> templates/ad_page.html 	deleted:    templates/index.html 	deleted:    templates/indi.html 	new file:   templates/sign.html 	new file:   templates/upload.html

--- manager_page/urls.py
+++ manager_page/urls.py
@@ -3,7 +3,10 @@ from django.urls import path , include
 from manager_page import views
 
 urlpatterns = [
-    path('', views.dash, name="dash"),
+    path('', views.admin_login, name="admin_login"),
     path('adminlogin',views.admin_login , name="adminlogin"),
-    path('add',views.add_emp , name='add')
+    path('add',views.add_emp , name='add'),
+    path('dash', views.dash , name="dash"),
+    path('add_emp' , views.add_emp, name='add_emp'),
+    path('upload',views.upload, name="upload"),
 ]
--- manager_page/views.py
+++ manager_page/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout , login
-
+import requests
 
 def dash(request):
-    if request.method=="POST" :
-      pass
-    return render(request,"admin_upload.html")
+    api_url = 'https://aatish13.pythonanywhere.com/api/admin/'
+    if request.method =="POST" :
+        name = request.POST["name"]        
+        image = request.POST["image"]
+        data = {
+            "name":name,
+            }
+        response=requests.post(api_url, data=data, files={"drawing": image})
+        if response.status_code == 201:
+            print('Name and image added successfully!')
+        else :
+            print("f")
+    return render(request,"ad_page.html")
 
 def admin_login(request) :
     if request.method =="POST" :
@@ -22,5 +32,48 @@ def admin_login(request) :
     return render(request ,"loginpage.html")
 
 def add_emp(request) :
-    return render(request,"indi.html")
+    api_url = 'https://aatish13.pythonanywhere.com/api/employees/'
+    if request.method =="POST" :
+        name = request.POST["name"]        
+        email = request.POST["email"]
+        phone_no = request.POST["phone_no"] 
+        data = {
+            "name":name,
+            "email":email,
+            "phone_no":phone_no,
+            }
+        response=requests.post(api_url, data=data)
+
+    return render(request,"sign.html")
    
+def sign(request) :
+    if request.method =="POST" :
+        name = request.POST["name"]
+        password = request.POST["password"]
+    return render(request, "sign.html")
+
+
+
+def upload(request):
+    api_url = 'https://aatish13.pythonanywhere.com/api/admin/'
+    
+    if request.method == "POST":
+        name = request.POST.get("name")  # Use the correct field name from your HTML form
+        image = request.FILES.get("image")  # Use the correct field name from your HTML form
+        
+        data = {
+            "name": name,
+        }
+        
+        files = {
+            "drawing": image,
+        }
+        
+        response = requests.post(api_url, data=data, files=files)
+        
+        if response.status_code == 201:
+            print('Name and image added successfully!')
+        else:
+            print("Error:", response.status_code)
+    
+    return render(request, "upload.html")
